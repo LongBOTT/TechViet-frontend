4b1d093e8d998f503f346dbc80e71350dad0d710
NguyenMinhThuan <95559097+NguyenMinhThuanK12@users.noreply.github.com>
2024-10-10 14:26:43 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './customer/components/NavBar'
+import { Login } from '@mui/icons-material'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <NavBar/>
+      <div><Login/></div>
       
     </div>
   )
