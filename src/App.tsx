454fb52fc6492f8ef81399c5fe0318d6d57c46b7
Nguyễn Hoàng Long <93486974+LongBOTT@users.noreply.github.com>
2024-10-10 13:12:16 +0700

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import NavBar from './customer/components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-
+    <div>
+      <NavBar/>
       
+    </div>
+  )
+}
+
+export default App
+
 
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -30,8 +37,3 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-    </div>
-  )
-}
-
-export default App
---
+++ src/customer/components/NavBar.tsx
@@ -0,0 +1,205 @@
+import { AppBar, Toolbar, IconButton, InputBase, Badge, Button, Box, Typography, styled, Container, Menu, MenuItem, Tooltip, Avatar, alpha, Link } from '@mui/material';
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MenuIcon from '@mui/icons-material/Menu';
+import { BorderBottom, BroadcastOnHome, Headphones, Laptop, PhoneAndroid, Tablet, Watch } from '@mui/icons-material';
+
+const categories = [
+  { name: 'Điện thoại', icon: PhoneAndroid },
+  { name: 'Máy tính bảng', icon: Tablet },
+  { name: 'Laptop', icon: Laptop },
+  { name: 'Đồng hồ thông minh', icon: Watch },
+  { name: 'Phụ kiện', icon: Headphones },
+  { name: 'Điện máy', icon: BroadcastOnHome },
+];
+const settings = ['Thông tin', 'Lịch sử mua hàng', 'Đăng xuất'];
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: 999,
+  backgroundColor: '#ffffff',
+  width: '100%'
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color:'#1c232d'
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: '#1c232d',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '500px',
+    },
+  },
+}));
+
+const ButtonCategory = styled(Button)(({ theme }) => ({
+  my: 2, 
+  color: 'white', 
+  display: 'flex', 
+  marginRight:'5px', 
+  fontSize:'12px',
+  borderRadius: '0px',
+  '&:hover': {
+    borderBottom: '2px solid white'
+  },
+}));
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" sx={{background: '#da031b', height:'100px'}}>
+      <Container>
+        <Toolbar sx={{alignItems: 'center', justifyContent: 'center'}}>
+          
+          {/* Logo */}
+          <Box component="img" src="src/assets/logo.png" alt="Logo" sx={{height: '100px', marginRight:'5px'}} />
+          
+          {/* Ẩn trong md */}
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="medium"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              {categories.map((category, index) => (
+                <MenuItem key={index} onClick={handleCloseNavMenu}>
+                  <category.icon />
+                  <Typography sx={{ textAlign: 'center' }}>{category.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          
+          {/* Ô tìm kiếm */}
+          <Box sx={{display: 'flex', flexDirection: 'column', paddingTop:{md:'2px'}, marginRight:'5px'}}>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Nhập tên điện thoại, máy tính, phụ kiện... cần tìm"
+                inputProps={{ 'aria-label': 'search'}}
+                sx={{fontSize: { xs: '10px', md: '15px'}}}
+              />
+            </Search>
+            
+            {/* Hiện danh sách thể loại trong md */}
+            <Box sx={{display: { xs: 'none', md: 'flex'}, marginTop:'10px', height:'40px', alignItems: 'center', justifyContent: 'center' }}>
+            {categories.map((category, index) => (
+              <ButtonCategory
+                key={index}
+                onClick={handleCloseNavMenu}
+              >
+                <category.icon sx={{ paddingRight:'5px', display: 'inline-flex'}}/>
+                {category.name}
+              </ButtonCategory>
+            ))}
+            </Box>
+          </Box>
+
+          {/* Hotline
+          <IconButton>
+            <PhoneInTalk sx={{height:'30px', width:'30px'}}/>
+          </IconButton>
+          <Link href="tel:18003355" sx={{color: 'white', fontSize:'15px'}}>18003355</Link> */}
+          
+          {/* Icon profile */}
+          <Box sx={{marginRight:'5px', marginLeft:'15px'}}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0} }>
+                <Avatar sx={{ background: '#7d161c', height:'30px', width:'30px'}}/>
+              </IconButton>
+            </Tooltip>
+
+            {/* Menu setting */}
+            <Menu
+              sx={{ mt: '45px', ml:'120px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {/* Hiện menu khi click */}
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center', fontSize: '15px'}}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Giỏ hàng */}
+          <IconButton>
+            <Badge badgeContent={2} color="secondary">
+              <ShoppingCartIcon sx={{height:'30px', width:'30px'}}/>
+            </Badge>
+          </IconButton>
+          <Typography sx={{ textAlign: 'center', fontSize: '15px'}}>Giỏ hàng</Typography>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default NavBar
