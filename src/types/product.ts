5ea6f3ea1e0611d167eafcb9b7ce69029d3d0525
Nguyễn Hoàng Long <93486974+LongBOTT@users.noreply.github.com>
2024-11-02 08:39:45 +0700

    Update product.ts

--- src/types/product.ts
+++ src/types/product.ts
@@ -5,15 +5,15 @@ import { Warranty } from "./warranty";
 
 export interface Product {
   id: number;        // Khoá chính, được sinh tự động
-  name: string;   
-  unit: string;   // Tên sản phẩm với độ dài 256 ký tự
-  image: string;     // Đường dẫn ảnh sản phẩm
+  name: string;      // Tên sản phẩm với độ dài 256 ký tự
   weight: number;    // Khối lượng sản phẩm
   description: string; // Mô tả sản phẩm với độ dài 4096 ký tự
   category: Category;  // Thể loại sản phẩm (liên kết với Category)
   brand: Brand;        // Thương hiệu (liên kết với Brand)
   warranty: Warranty;  // Bảo hành sản phẩm (liên kết với Warranty)
+  image: string
   status: string;      // Trạng thái sản phẩm (đang giao dịch, ngưng giao dịch,đã xóa)
+  unit: string;   // Tên sản phẩm với độ dài 256 ký tự
 }
 
 export interface ProductRequest {
